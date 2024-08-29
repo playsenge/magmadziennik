c85a760ddffdb30d90e9112b18bc8b2d1e72454d
senge1337 <playsenge@gmail.com>
2024-08-29 12:55:54 +0200

    login

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 
 export default function Footer() {
-    return (
-        <footer className = "quicksand flex h-[7vh] items-center gap-7 bg-gray-300 px-6 text-slate-500">
-
-            <Link className="text-3xl" to="/">MAGMA</Link>
-            <Link className="ml-auto" to="/rodo">RODO</Link>
-            <Link to= "/contact">Kontakt</Link>
-            <Link to="/about-us">O nas</Link>
-            <Link to="/privacy-policy">Polityka prywatności</Link>
-
-            </footer>
-    );
+  return (
+    <footer className="quicksand flex h-[7vh] items-center gap-7 bg-gray-300 px-6 text-slate-500">
+      <Link className="text-3xl" to="/">
+        MAGMA
+      </Link>
+      <Link className="ml-auto" to="/rodo">
+        RODO
+      </Link>
+      <Link to="/contact">Kontakt</Link>
+      <Link to="/about-us">O nas</Link>
+      <Link to="/privacy-policy">Polityka prywatności</Link>
+    </footer>
+  );
 }
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -7,27 +7,44 @@ import { FaLock } from "react-icons/fa";
 import Footer from "../components/footer";
 
 export default function LoginPage() {
-    const emailInput = useRef<HTMLInputElement>(null);
-    const passwordInput = useRef<HTMLInputElement>(null);
+  const emailInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
 
-    return (
-        <><div className="flex h-[93vh] items-center justify-center">
-            <div className="flex flex-col items-center justify-center gap-5 rounded-xl bg-gray-700 p-12 text-white shadow-xl shadow-slate-800">
-                <h1 className="text-4xl font-bold">Zaloguj się</h1>
-                <Input className="bg-slate-600" type="email" placeholder="E-mail" ref={emailInput} icon={<MdOutlineAlternateEmail />} />
-                <Input type="password" placeholder="Hasło" ref={passwordInput} icon={<FaLock />} />
-                <Link to="/forgot-password" className="underline">Zapomniałeś hasła?</Link>
-                <Button onClick={() => {
-                    if (!emailInput.current || !passwordInput.current)
-                        return;
+  return (
+    <>
+      <div className="flex h-[93vh] items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-5 rounded-xl bg-gray-700 p-12 text-white shadow-xl shadow-slate-800">
+          <h1 className="text-4xl font-bold">Zaloguj się</h1>
+          <Input
+            className="bg-slate-600"
+            type="email"
+            placeholder="E-mail"
+            ref={emailInput}
+            icon={<MdOutlineAlternateEmail />}
+          />
+          <Input
+            type="password"
+            placeholder="Hasło"
+            ref={passwordInput}
+            icon={<FaLock />}
+          />
+          <Link to="/forgot-password" className="underline">
+            Zapomniałeś hasła?
+          </Link>
+          <Button
+            onClick={() => {
+              if (!emailInput.current || !passwordInput.current) return;
 
-                    const email = emailInput.current.value;
-                    const password = passwordInput.current.value;
-                    alert(`email: ${email}`);
-                }}>Zaloguj się</Button>
-            </div>
+              const email = emailInput.current.value;
+              const password = passwordInput.current.value;
+              alert(`email: ${email}`);
+            }}
+          >
+            Zaloguj się
+          </Button>
         </div>
-        <Footer />
-            </>
-    );
+      </div>
+      <Footer />
+    </>
+  );
 }
