28cc5fd66f6e8ed05eeffed82ab22dd3c718be6f
duck002 <lukaszbaka124@gmail.com>
2024-08-29 23:17:41 +0200

    Added home tab and Skibidibian language for the Gen Alpha minority

--- frontend/src/pages/PanelPage/tabs/HomeTab.tsx
+++ frontend/src/pages/PanelPage/tabs/HomeTab.tsx
@@ -1,3 +1,33 @@
+import { msg } from "../../../language";
+
 export default function HomeTab() {
-  return <>Home page!</>;
+  return <>
+    <div className="">
+    <div className="grid grid-cols-3 gap-4">
+  <div className="h-64 rounded-2xl bg-white">
+    <h1 className="m-3 text-4xl">{msg.universal.grades}</h1>
+  </div>
+  <div className="h-64 rounded-2xl bg-white">
+  <h1 className="m-3 text-4xl">{msg.tabs.tests}</h1>
+  </div>
+  <div className="h-64 rounded-2xl bg-white">
+    <h1 className="m-3 text-4xl">{msg.tabs.homework}</h1>
+  </div>
+  <div className="col-span-2 h-64 rounded-2xl bg-white">
+  <h1 className="m-3 text-4xl">{msg.universal.timetable}</h1>
+  </div>
+  <div className="h-64 rounded-2xl bg-white">
+  <h1 className="m-3 text-4xl">{msg.tabs.attendance}</h1>
+
+  </div>
+  <div className="h-64 rounded-2xl bg-white">
+  <h1 className="m-3 text-4xl">{msg.tabs.messages}</h1>
+  </div>
+  <div className="col-span-2 h-64 rounded-2xl bg-white">
+  <h1 className="m-3 text-4xl">idk</h1>
+  </div>
+</div>
+
+    </div>
+  </>;
 }
