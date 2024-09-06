6c3f0dee5e9a4b6260cd2e06a3e261f3b6a2596d
senge1337 <40773107+playsenge@users.noreply.github.com>
2024-09-06 21:38:38 +0200

    Update utils.js

--- backend/pb_hooks/utils.js
+++ backend/pb_hooks/utils.js
@@ -5,7 +5,7 @@ module.exports = {
     fix: (data) => {
         return JSON.parse(JSON.stringify(data));
     }, // Monkeypatch for data not working for some reason
-    teacherFilter: (e, items, fix) => {
+    teacherGradesAccessFilter: (e, items, fix) => {
         // If accessing through PocketBase admin panel, ignore
         if (e.httpContext.get("admin") !== null)
             return items;
@@ -40,5 +40,38 @@ module.exports = {
         });
 
         return filteredItems;
-    }
+    },
+    teacherClassesAccessFilter: (e, items, fix) => {
+        // If accessing through PocketBase admin panel, ignore
+        if (e.httpContext.get("admin") !== null)
+            return items;
+
+        const user = fix(e.httpContext.get("authRecord"));
+
+        // Students viewing their own classes is secure by default
+        if (user.collectionName !== "teachers")
+            return items;
+
+        // Admin teachers can view all of the data
+        if (user.admin === true)
+            return items;
+
+        const filteredItems = [];
+
+        items.forEach((ogItem) => {
+            const item = fix(ogItem);
+
+            // If teacher doesn't teach in that class, they can't view it
+            // TODO: Make it only show the name instead (for searching students
+            // in Messages class will have to fetched to view for example Foo Bar
+            // (3d) but other data should stay censored)
+            if (!Object.keys(item.teacher_subject_pairs).includes(user.id)) {
+                return;
+            }
+
+            filteredItems.push(ogItem);
+        });
+
+        return filteredItems;
+    },
 }
